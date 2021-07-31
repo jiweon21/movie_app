f0815e99e273573217c93ce36f24d636e85f41f9
jiweon21 <jna215843@gmail.com>
2021-07-31 22:28:08 +0900

    edit:movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+  return (
+    <>
+      <h4>{title}</h4>
+      <h4>{year}</h4>
+      <h4>{summary}</h4>
+      <h4>{poster}</h4>
+    </>
+  );
 }
 
 export default Movie;
